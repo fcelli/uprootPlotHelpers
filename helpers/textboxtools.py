80796cfdadea3a5cd8866214be4b29e04329999d
fcelli <fedecel93@gmail.com>
2020-07-27 16:41:07 +0200

    Add textboxtools.

---
+++ helpers/textboxtools.py
@@ -0,0 +1,61 @@
+experiment = None
+internal   = True
+sim        = True
+ecom       = None
+lumi       = None
+text       = None
+pos        = (0.05,0.95)
+fontsize   = 14
+
+def Experiment(exp):
+  global experiment
+  experiment = exp
+
+def Internal(is_internal):
+  global internal
+  internal = is_internal
+
+def Simulation(is_sim):
+  global sim
+  sim = is_sim
+
+def Ecom(e):
+  global ecom
+  ecom = e
+
+def Luminosity(l):
+  global lumi
+  lumi = l
+
+def Text(t):
+  global text
+  text = t
+
+def Position(x,y):
+  global pos
+  pos = (x,y)
+
+def FontSize(f):
+  global fontsize
+  fontsize = f
+
+def MakeTextBox(): 
+  textbox = ''
+  if experiment is not None:
+    textbox += ('$\mathbf{'+str(experiment)+'}$')
+    if internal:
+      textbox += ' $Internal$'
+    textbox += '\n'
+  if sim:
+    textbox += 'Simulation '
+  if ecom is not None:
+    textbox += ('$\sqrt{'+str(ecom)+'}TeV$ ')
+  if lumi is not None:
+    textbox += (str(lumi)+'$fb^{-1}$')
+  if text is not None:
+    textbox += '\n{}'.format(text)
+  return textbox
+
+
+
+
